59da95cf588b5d07c3e6ebc32ad01233435e5ce6
igorfs10 <igorfs10@gmail.com>
2021-06-24 13:36:02 -0300

    remove uneeded code

--- business/src/service/usuario_service.rs
+++ business/src/service/usuario_service.rs
@@ -10,37 +10,22 @@ pub struct UsuarioService;
 
 impl UsuarioService {
     pub async fn get_all() -> Result<Vec<Usuario>, String> {
-        match Service::get_all().await {
-            Ok(result) => Ok(result),
-            Err(error) => Err(error),
-        }
+        Service::get_all().await
     }
 
     pub async fn get_by_id(id: i64) -> Result<Usuario, String> {
-        match Service::get_by_id(id).await {
-            Ok(result) => Ok(result),
-            Err(error) => Err(error),
-        }
+        Service::get_by_id(id).await
     }
 
     pub async fn insert(usuario: Usuario) -> Result<String, String> {
-        match Service::insert(usuario).await {
-            Ok(_) => Ok("Usuário criado com sucesso.".to_string()),
-            Err(error) => Err(error),
-        }
+        Service::insert(usuario).await
     }
 
     pub async fn update(usuario: Usuario) -> Result<String, String> {
-        match Service::update(usuario).await {
-            Ok(_) => Ok("Usuário atualizado com sucesso.".to_string()),
-            Err(error) => Err(error),
-        }
+        Service::update(usuario).await
     }
 
     pub async fn delete(id: i64) -> Result<String, String> {
-        match Service::delete(id).await {
-            Ok(_) => Ok("Usuário excluído com sucesso.".to_string()),
-            Err(error) => Err(error),
-        }
+        Service::delete(id).await
     }
 }
